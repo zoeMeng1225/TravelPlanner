d9fb1cd3086183bb488072a2157a1653d010442d
Zoe Meng <zoebo1225@gmail.com>
2020-11-03 16:39:45 -0800

    updated

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,1 @@
-export const Travel_Plan_BASE_URL = "http://travelplanner-env.eba-mcadpdmq.us-east-2.elasticbeanstalk.com/";
+export const Travel_Plan_BASE_URL = "https://travelplanner-env.eba-mcadpdmq.us-east-2.elasticbeanstalk.com/";
